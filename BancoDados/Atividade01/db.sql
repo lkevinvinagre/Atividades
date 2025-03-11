1b9d4cb7e32e0025812c565d11d8d6021c536d76
KevinNot\Kevin <lkevinvinagre@hotmail.com>
2025-03-11 11:32:04 -0400

    update

--- BancoDados/Atividade01/db.sql
+++ BancoDados/Atividade01/db.sql
@@ -1,26 +1,26 @@
 -- Tables
 CREATE TABLE IF NOT EXISTS tables(
-    id LONG NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     locations VARCHAR(160) NOT NULL,
     hourValues DOUBLE NOT NULL
 );
 -- Clients
 CREATE TABLE IF NOT EXISTS clients(
-    id LONG NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     names VARCHAR(160) NOT NULL,
     email VARCHAR(160) NOT NULL
 );
 -- Products
 CREATE TABLE IF NOT EXISTS products(
-    id LONG NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     names VARCHAR(160) NOT NULL,
     price DOUBLE NOT NULL
 );
 -- Schedulings
 CREATE TABLE IF NOT EXISTS schedulings(
-    id LONG NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tId LONG NOT NULL,
-    cId LONG NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tId INT NOT NULL,
+    cId INT NOT NULL,
     dates DATE NOT NULL DEFAULT CURRENT_DATE,
     status ENUM ('reserved', 'canceled', 'open','payment','closed') NOT NULL DEFAULT 'reserved',
     FOREIGN KEY (tId) REFERENCES tables(id),
@@ -28,8 +28,8 @@ CREATE TABLE IF NOT EXISTS schedulings(
 );
 -- Product schedules
 CREATE TABLE IF NOT EXISTS productsche(
-    pId LONG NOT NULL,
-    eId LONG NOT NULL,
+    pId INT NOT NULL,
+    eId INT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (pId) REFERENCES products(id),
     FOREIGN KEY (eId) REFERENCES schedulings(id),
